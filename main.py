ef91ab8705ed12fffe5ee4cf34164aec55747279
Krishn Mukesh Navadiya <krishnmukeshnavadiya@Krishn-MacBook-Pro.local>
2026-01-27 14:02:34 +0530

    Add greet utility function

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 print("Hello, Git!");
 print("Changes for new commit");
+print("git init");
+print("git add main.py");
+print("git commit -m 'Add initial hello world script'");
+print("git add Main.py");
+print("git commit -m 'Update script with learning message'");
+
+def greet(name):
+    return f"Hello, {name}"
